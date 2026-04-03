2292d81f555c96a6f03d18c2ba5657054060eaca
Daniel Palmer <daniel@0x0f.com>
2026-04-03 20:37:54 +0900

    deadtest rom bits

---
+++ deadtestrom/m68mx.h
@@ -0,0 +1,12 @@
+#define CRTC		0xe30001
+#define PTM		0xe30081
+#define PTM_CR1_CR3	0
+#define PTM_CR2		2
+#define PTM_CR3		4
+
+#define ACIA1_CS	0xe30161
+#define ACIA1_DATA	0xe30163
+#define ACIA2_CS	0xe30141
+#define ACIA2_DATA	0xe30143
+#define TEXTVRAM	0xe10000
+#define TEXTVRAMSZ	0x2000
